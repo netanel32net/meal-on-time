b4bb726d23c81f68037663d00e88e74c4528b5c8
netanel32net <fun22019@gmail.com>
2022-11-13 00:03:20 +0200

    add-row

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ function Header() {
       {/* <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" className="d-inline-block
        align-text-top"/> */}
        <i className="bi-cup-hot me-2"></i>
-      Meal On Time
+      Meal On Timeeeeee
     </a>
   </div>
 </nav>
