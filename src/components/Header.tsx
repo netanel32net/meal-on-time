921fef28bf4dd3781741ac26c1bcc543bacb9ac2
netanel32net <fun22019@gmail.com>
2022-11-12 23:59:30 +0200

    init-react

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+<nav className="navbar navbar-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      {/* <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" className="d-inline-block
+       align-text-top"/> */}
+       <i className="bi-cup-hot me-2"></i>
+      Meal On Time
+    </a>
+  </div>
+</nav>
+    );
+}
+
+export default Header;
